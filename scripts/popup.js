0d706f20ca2ad4701a9a5720b307c0676222a2bf
gatalsky-dv <gatalsky.d.v@gmail.com>
2022-04-28 16:37:45 +0500

    feature/js at work

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,7 +1,7 @@
 const buttonOpenBtn = document.querySelector('.button__edit');
 const popup = document.querySelector('.popup');
 const buttonCloseBtn = document.querySelector('.button__close');
-const buttonSaveBtn = document.querySelector('.button__save')
+// const buttonSaveBtn = document.querySelector('.button__save')
 
 buttonOpenBtn.addEventListener('click', function (event) {
   event.preventDefault();
@@ -18,12 +18,11 @@ buttonCloseBtn.addEventListener('click', function (event) {
 // ----------------------------------------
 
 // Находим форму в DOM
-let formElement = document.querySelector('.popup'); // Воспользуйтесь методом querySelector()
+let formElement = document.querySelector('.popup__container'); // Воспользуйтесь методом querySelector()
 // Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__name'); // Воспользуйтесь инструментом .querySelector()
 let jobInput = document.querySelector('.popup__description'); // Воспользуйтесь инструментом .querySelector()
 
-// console.log(document.querySelector('.profile__name'));
  console.log(nameInput.value);
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -44,10 +43,10 @@ function formSubmitHandler (evt) {
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler); 
 
-buttonSaveBtn.addEventListener('click', function (event) {
-  const currentPopup = event.target.closest('.popup');
-  currentPopup.classList.toggle('popup_opened');
-  document.body.style.overflowY = '';
-  formSubmitHandler;
+// buttonSaveBtn.addEventListener('click', function (event) {
+//   const currentPopup = event.target.closest('.popup');
+//   currentPopup.classList.toggle('popup_opened');
+//   document.body.style.overflowY = '';
+//   formSubmitHandler;
 
-});
+// });
