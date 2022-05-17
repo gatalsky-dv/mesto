0c7547e43060e4ab5190cdd00477998f7759254e
gatalsky-dv <gatalsky.d.v@gmail.com>
2022-05-17 20:56:02 +0500

    popup button done

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,11 @@ const popupContainer = document.querySelector('.popup__container');
 const popupImg = document.querySelector('.popup__img');
 const popupText = document.querySelector('.popup__text');
 
+const elementTemplate = document.querySelector('#element-template').content;
+const elementContainer = document.querySelector('.elements');
+const elementCard = elementTemplate.querySelector('.element');
+const elementMaskgroup = elementCard.querySelector('.element__maskgroup');
+const elementName = elementCard.querySelector('.element__name');
 
 // Находим форму в DOM
 const popupEdit = document.querySelector('.popup__edit'); // Воспользуйтесь методом querySelector()
@@ -52,56 +57,44 @@ function closePopup() {
   popupSave.classList.remove('popup__save_opened');
   popupTitle.textContent = '';
   popupSave.textContent = '';
-  
-}
-
-function closeEditProfilePopup() {
   popupInputValueName.classList.remove('popup__input_opened');
   popupInputValueJob.classList.remove('popup__input_opened');
-
   popupInputValueTitle.classList.remove('popup__input_opened');
   popupInputValueLink.classList.remove('popup__input_opened');
-  closePopup();
   popup.classList.remove('popup_opened');
   popupImg.classList.remove('popup__img_opened');
   popupContainer.classList.remove('popup__container_opened');
   popupText.classList.remove('popup__text_opened');
   popupInputValueTitle.value = '';
   popupInputValueLink.value = '';
-}
-
-function closeAddCardPopup() {
   popupInputValueTitle.classList.remove('popup__input_opened');
   popupInputValueLink.classList.remove('popup__input_opened');
   popupInputValueTitle.value = '';
   popupInputValueLink.value = '';
-  closePopup();
 }
 
 profileEditButton.addEventListener('click', openEditProfilePopup);
-profileCloseButton.addEventListener('click', closeEditProfilePopup);
+profileCloseButton.addEventListener('click', closePopup);
 cardAddButton.addEventListener('click', openAddCardPopup);
-// cardAddButton.addEventListener('click', closeAddCardPopup);
+
+const popupFunctionClickButton = (evt) => {
+  if (popupSave.textContent === 'Сохранить') {
+    formSubmitEditProfilePopup(evt);
+  }
+  if (popupSave.textContent === 'Создать') {
+    formSubmitCardPopup(evt);
+  }
+}
 
 function formSubmitEditProfilePopup (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
 
   profileName.textContent = popupInputValueName.value;
   profileDescription.textContent = popupInputValueJob.value;
-  closeEditProfilePopup();
+  closePopup();
 }
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-// popupEdit.addEventListener('submit', formSubmitEditProfilePopup);
 
-//------------------ Практическая работа №5 --------------------
-
-
-const elementTemplate = document.querySelector('#element-template').content;
-const elementContainer = document.querySelector('.elements');
-const elementCard = elementTemplate.querySelector('.element');
-const elementMaskgroup = elementCard.querySelector('.element__maskgroup');
-const elementName = elementCard.querySelector('.element__name');
+popupEdit.addEventListener('submit', popupFunctionClickButton);
 
 const renderCard = (item) => {
   elementCard.cloneNode(true);
@@ -119,16 +112,13 @@ initialCards.forEach((item) => {
   elementContainer.append(elementCard.cloneNode(true));
 });
 
-
-
-function cardSubmitHandler (evt) {
+function formSubmitCardPopup (evt) {
   evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
   createCard({name: popupInputValueTitle.value, link: popupInputValueLink.value});
   elementContainer.prepend(elementCard.cloneNode(true));
-  closeAddCardPopup();
+  closePopup();
 }
 
-popupEdit.addEventListener('submit', cardSubmitHandler);
 
 const cardDelete = (el) => {
   el.closest('.element').remove();
