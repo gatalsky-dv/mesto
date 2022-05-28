f313fe611cdbfaacbbfe5b2fa94afda458662f8f
gatalsky-dv <gatalsky.d.v@gmail.com>
2022-05-28 23:47:53 +0500

    text error

---
+++ scripts/validate.js
@@ -0,0 +1,9 @@
+// enableValidation({
+//   formSelector: '.popup__form',                   //.popup__edit
+//   inputSelector: '.popup__input',                 //.popup__input
+//   submitButtonSelector: '.popup__button',         //.popup__save
+//   inactiveButtonClass: 'popup__button_disabled',  //.popup__save_disabled
+//   inputErrorClass: 'popup__input_type_error',     //popup__input_type_error
+//   errorClass: 'popup__error_visible'              //popup__error_visible
+// }); 
+
